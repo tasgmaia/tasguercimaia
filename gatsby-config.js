d9237caac220484ce0dd761ee683e4e07aa1e13b
Tas <tas.g.maia@gmail.com>
2020-12-24 01:19:12 -0300

    visual updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,10 +21,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#a5a5a5`,
+        theme_color: `#0007dd`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/tas-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
