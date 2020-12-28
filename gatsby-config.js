4d4d03342bbfcc926cffd8fc457a8b7416380a24
Tas <tas.g.maia@gmail.com>
2020-12-28 15:25:23 -0300

    add gtm

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,36 @@ module.exports = {
     author: `@TasGuerciMaia`,
   },
   plugins: [
+    //TAG MANAGER CONFIG
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-THG39ZV",
+  
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+  
+        // Specify optional GTM environment details.
+        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        dataLayerName: "YOUR_DATA_LAYER_NAME",
+  
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+      },
+    },
+    //HELMET CONFIG
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
